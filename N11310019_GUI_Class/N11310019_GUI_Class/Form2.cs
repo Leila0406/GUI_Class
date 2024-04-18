f409e797d1aae082588c708a113a27c94838f505
Leila0406 <lark930406@gmail.com>
2024-04-18 21:03:12 +0800

    cf_20240418

---
+++ N11310019_GUI_Class/N11310019_GUI_Class/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace N11310019_GUI_Class
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string tmp = textBox1.Text;
+            int num = C = (F)* 9 / 5 + 32;
+            textBox1.Text = num.ToString();
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+}
